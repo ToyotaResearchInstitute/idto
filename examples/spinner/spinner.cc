53245cc5be78395932e88b48f46a5e0e871187a5
Vince Kurtz <vjkurtz@gmail.com>
2023-09-28 13:37:54 -0700

    adopt minimum linting capabilities from drake

--- examples/spinner/spinner.cc
+++ examples/spinner/spinner.cc
@@ -1,8 +1,8 @@
 #include "examples/example_base.h"
+#include "utils/find_resource.h"
 
 #include <drake/multibody/parsing/parser.h>
 #include <drake/multibody/plant/multibody_plant.h>
-#include "utils/find_resource.h"
 
 namespace idto {
 namespace examples {
@@ -26,8 +26,8 @@ class SpinnerExample : public TrajOptExample {
     // N.B. geometry of the spinner is chosen via gflags rather than yaml so
     // that we can use the same yaml format for all of the examples, without
     // cluttering it with spinner-specific options.
-    std::string urdf_file = idto::FindIDTOResourceOrThrow(
-        "examples/models/spinner_friction.urdf");
+    std::string urdf_file =
+        idto::FindIDTOResourceOrThrow("examples/models/spinner_friction.urdf");
     Parser(plant).AddModels(urdf_file);
   }
 };
