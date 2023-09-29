ea39a4d6b1d8076002186479f875a97a59056079
Vince Kurtz <vjkurtz@gmail.com>
2023-09-29 10:11:36 -0700

    set up separate test case for acrobot

--- examples/acrobot/acrobot.cc
+++ examples/acrobot/acrobot.cc
@@ -3,6 +3,11 @@
 #include <drake/common/find_resource.h>
 #include <drake/multibody/parsing/parser.h>
 #include <drake/multibody/plant/multibody_plant.h>
+#include <gflags/gflags.h>
+
+DEFINE_bool(test, false,
+            "whether this example is being run in test mode, where we solve a "
+            "simpler problem");
 
 namespace idto {
 namespace examples {
@@ -32,10 +37,15 @@ class AcrobotExample : public TrajOptExample {
 }  // namespace examples
 }  // namespace idto
 
-int main() {
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   idto::examples::acrobot::AcrobotExample example;
-  const std::string yaml_file = "examples/acrobot/acrobot.yaml";
-  example.RunExample(yaml_file);
+  if (FLAGS_test) {
+      example.RunExample("examples/acrobot/test.yaml");
+  } else {
+      example.RunExample("examples/acrobot/acrobot.yaml");
+  }
 
   return 0;
 }
