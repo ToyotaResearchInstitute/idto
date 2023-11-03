29678f87ec47b39cd3f07c1ee2bb14ac9bec8357
Vince Kurtz <vjkurtz@gmail.com>
2023-11-03 09:18:28 -0700

    clean up simple example

--- bindings/adder.cc
+++ bindings/adder.cc
@@ -1,12 +1,14 @@
 #include <pybind11/pybind11.h>
 
-namespace {
+int add(int x, int y) {
+  return x + y;
+}
 
-  int add(int x, int y) { return x + y; }
-
+int subtract(int x, int y) {
+  return x - y;
 }
 
 PYBIND11_MODULE(adder, m) {
   m.def("add", &add, "adds two numbers");
+  m.def("subtract", &subtract, "subtracts two numbers");
 }
-
--- bindings/adder_test.py
+++ bindings/adder_test.py
@@ -1,15 +1,9 @@
-import os
-import sys
-
-print('about to import', file=sys.stderr)
-print('python is', sys.version_info)
-print('pid is', os.getpid())
-
 from bindings import adder
 
-print('imported, about to call', file=sys.stderr)
-
 result = adder.add(2, 3)
+print("sum of 2 and 3 is ", result)
 assert result == 5
 
-print('done!', file=sys.stderr)
+result = adder.subtract(2, 3)
+print("difference of 2 and 3 is ", result)
+assert result == -1
